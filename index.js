ebf320b1b70a57a1e42e0e9dbb875fad2a7ba172
olxzr <olxazar96@mail.ru>
2024-02-02 00:27:55 +0300

    new

---
+++ index.js
@@ -0,0 +1,30 @@
+// alert("Я JavaScript!");
+
+
+// ПЕРВОЕ ЗАДАНИЕ
+
+// создал переменную 
+let age = 200;
+
+// переименовал переменную
+age = 1997;
+
+
+// console.log(age);
+
+
+
+let Name = prompt('Студент');
+
+let address = prompt('Адрес');
+
+let phone = prompt('Телефон');
+
+
+
+console.log('Студент' + ' ' + Name + '\n' + 'Адрес' + ' ' + address  + '\n' + 'Телефон' + ' ' + phone );
+
+
+
+
+
