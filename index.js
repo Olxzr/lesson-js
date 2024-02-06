fb5e156a404472de3ae186c38161db29843c7f60
olxzr <olxazar96@mail.ru>
2024-02-06 11:03:41 +0300

    сделал 3 задания по LS

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
-// alert("Я JavaScript!");
+// // ПЕРВОЕ ЗАДАНИЕ
 
+// // создал переменную 
+// let age = 200;
 
-// ПЕРВОЕ ЗАДАНИЕ
+// // переименовал переменную
+// age = 1997;
 
-// создал переменную 
-let age = 200;
+// // console.log(age);
 
-// переименовал переменную
-age = 1997;
+// let Name = prompt('Студент');
 
+// let address = prompt('Адрес');
 
-// console.log(age);
+// let phone = prompt('Телефон');
 
-
-
-let Name = prompt('Студент');
-
-let address = prompt('Адрес');
-
-let phone = prompt('Телефон');
+// console.log('Студент' + ' ' + Name + '\n' + 'Адрес' + ' ' + address  + '\n' + 'Телефон' + ' ' + phone );
 
 
 
-console.log('Студент' + ' ' + Name + '\n' + 'Адрес' + ' ' + address  + '\n' + 'Телефон' + ' ' + phone );
 
+// Первое ЗАДАНИЕ
 
+function addition(){
+    let a = 5;
+    let c = 10 + a;
+    console.log(c);
+}
+addition()
+
+// Второе ЗАДАНИЕ
+
+function multiplication(){
+    let a = 5;
+    let c = 10 * a;
+    console.log(c);
+}
+multiplication()
+
+// Третье ЗАДАНИЕ
+
+function CheckingForPositivity(){
+    let a = 10;
+    let c = a > 5;
+    console.log(c)
+}
+CheckingForPositivity()
 
 
 
