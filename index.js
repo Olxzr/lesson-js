19dc8a5f482bd01e979f030d9994c40c4067e2e4
olxzr <olxazar96@mail.ru>
2024-02-07 16:51:06 +0300

    дошел до 6го задания

--- index.js
+++ index.js
@@ -21,6 +21,14 @@
 
 // Первое ЗАДАНИЕ
 
+function asa(o, m){
+    return o + m;
+}
+let res = asa(1,2)
+console.log(res)
+asa(1, 2)
+
+
 function addition(){
     let a = 5;
     let c = 10 + a;
@@ -28,7 +36,15 @@ function addition(){
 }
 addition()
 
-// Второе ЗАДАНИЕ
+// // Второе ЗАДАНИЕ
+
+function asaf(o, m){
+    return o * m;
+}
+let resg = asaf(10,2)
+console.log(resg)
+asa(10, 2)
+
 
 function multiplication(){
     let a = 5;
@@ -37,14 +53,89 @@ function multiplication(){
 }
 multiplication()
 
-// Третье ЗАДАНИЕ
+// // Третье ЗАДАНИЕ
 
-function CheckingForPositivity(){
-    let a = 10;
-    let c = a > 5;
-    console.log(c)
+// function CheckingForPositivity(ten){
+//     if(ten >= 18) 
+//         alert('достут полчен')
+//     else alert('доступ закрыт')
+// }
+// CheckingForPositivity(17)
+
+// Второй способ третьего задания
+
+let two = 10;
+if(two > 15)
+{ console.log('Переменная больше 15') }
+else(two < 15)
+    console.log("Переменная меньше 15")
+
+
+// четвертое ЗАДАНИЕ
+
+let fifteen = 15;
+
+let seven = 7;
+
+let result = fifteen + seven;
+
+console.log(result)
+
+// пятое ЗАДАНИЕ
+
+let a = true;
+
+let b = false;
+
+let c = a > b;
+
+let d = a < b;
+
+let i = a == b;
+
+let f = a != b;
+
+console.log(a, b, c, i, f);
+
+// шестое ЗАДАНИЕ
+
+function AgeVerification(age){
+    if(age >= 18) 
+        console.log('Подросток или взрослый')
+    else console.log('ребенок')
 }
-CheckingForPositivity()
+AgeVerification(18)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let login = 'Adlan1';
+
+// let password = 'waw';
+
+// let getlogin = prompt('Ведите Логин')
+
+// let rugust = (getlogin === login) ? console.log('Успешно') : console.log('не верно!')
+
+
+
+// console.log(rugust);
